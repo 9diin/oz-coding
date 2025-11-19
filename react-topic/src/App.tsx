9844d59304093f8758486de9dbe6702d757ffa67
9diin <weartstudio@naver.com>
2025-11-19 10:57:10 +0900

    [feature] 검색창 UI 완성

--- react-topic/src/App.tsx
+++ react-topic/src/App.tsx
@@ -1,5 +1,5 @@
-import { ChartNoAxesCombined, ChevronDown, CodeXml, DraftingCompass, Footprints, Goal, Icon, Lightbulb, List, Rocket } from "lucide-react";
-import { Button } from "./components/ui";
+import { ChartNoAxesCombined, ChevronDown, CodeXml, DraftingCompass, Footprints, Goal, Icon, Lightbulb, List, Rocket, Search } from "lucide-react";
+import { Button, Input } from "./components/ui";
 
 const CATEGORIES = [
     // { icon: List, label: "전체" },
@@ -36,9 +36,24 @@ function App() {
                     })}
                 </div>
             </aside>
-            <div className="min-h-screen flex-1 bg-amber-500 flex flex-col gap-12">
-                {/* 검색창 */}
-                <section>검색창 섹션</section>
+            <div className="min-h-screen flex-1 flex flex-col gap-12">
+                <section className="w-full flex flex-col items-center justify-center gap-6">
+                    <div className="flex flex-col">
+                        <div className="flex items-center gap-2">
+                            <img src="/gifs/heart.gif" alt="@HEART_GIFS" className="w-8" />
+                            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">지식과 인사이트를 모아,</h3>
+                        </div>
+                        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">토픽으로 깊이 있게 나누세요!</h3>
+                    </div>
+                    {/* 검색창 */}
+                    <div className="w-full max-w-lg flex items-center gap-2 border py-2 pl-4 pr-3 rounded-full">
+                        <Search size={24} className="text-neutral-500 -mr-2" />
+                        <Input placeholder="관심 있는 클래스, 토픽 주제를 검색하세요." className="border-none bg-transparent! focus-visible:ring-0 placeholder:text-base" />
+                        <Button variant={"secondary"} className="rounded-full">
+                            검색
+                        </Button>
+                    </div>
+                </section>
                 {/* HOT 토픽 */}
                 <section>HOT 토픽 섹션</section>
                 {/* NEW 토픽 */}

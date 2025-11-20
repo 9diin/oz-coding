5d88f531d2aea551219831b1663c52c56debe70d
9diin <weartstudio@naver.com>
2025-11-20 13:43:51 +0900

    [feature] Create-Topic 썸네일 업로드 기능 작업 中

--- react-topic/src/pages/topic/create-topic.tsx
+++ react-topic/src/pages/topic/create-topic.tsx
@@ -1,8 +1,34 @@
+import { useRef, useState } from "react";
 import { AppTextEditor } from "@/components/common";
 import { Button, Input, Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue, Separator } from "@/components/ui";
 import { Asterisk, Image, ImageOff } from "lucide-react";
 
 function CreateTopic() {
+    const [title, setTitle] = useState<string | null>(null);
+    const [content, setContent] = useState(null);
+    const [category, setCategory] = useState<string | null>(null);
+    const [thumbnail, setThumbnail] = useState<File | string | null>(null);
+    // => File 타입의 원본 데이터를 받음
+    // => Supabase의 이미지만 관리하는 Storage에 전달받은 File을 저장 => URL 형식으로
+    // => Supabase 데이터베이스에 저장 (in topics 테이블의 thumbnail 컬럼)
+
+    const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+    // 파일 변화 감지 및 상태값 할당
+    const handleChangeFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+        // if (event.target.files) {
+        //     setThumbnail(event.target.files[0]);
+        // } else {
+        //     setThumbnail(null);
+        // }
+        setThumbnail(event.target.files?.[0] ?? null);
+
+        console.log(event.target.files);
+
+        // 동일 파일 선택이 불가능할 수 있으므로 event.target.value를 초기화
+        event.target.value = "";
+    };
+
     return (
         <main className="w-full flex-1 flex justify-center">
             <div className="w-full max-w-[1328px] h-full flex gap-6 py-6">
@@ -74,12 +100,13 @@ function CreateTopic() {
                             </div>
                             <div className="flex flex-col gap-2">
                                 <div className="w-full aspect-video flex items-center justify-center rounded-md bg-card">
-                                    <Button variant={"ghost"} size={"icon"}>
+                                    <Button variant={"ghost"} size={"icon"} onClick={() => fileInputRef.current?.click()}>
                                         <Image />
                                     </Button>
+                                    <Input type="file" accept="image/*" ref={fileInputRef} onChange={handleChangeFile} className="hidden" />
                                 </div>
                                 {/* 썸네일 제거 버튼 */}
-                                <Button variant={"secondary"} className="bg-card">
+                                <Button variant={"secondary"} className="bg-card" onClick={() => setThumbnail(null)}>
                                     <ImageOff />
                                     썸네일 제거
                                 </Button>

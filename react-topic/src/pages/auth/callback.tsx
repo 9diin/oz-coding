62a9f88386082a376b45a918ef034edb9fd5274e
9diin <weartstudio@naver.com>
2025-11-28 13:13:34 +0900

    7트

--- react-topic/src/pages/auth/callback.tsx
+++ react-topic/src/pages/auth/callback.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router";
+// import { useNavigate } from "react-router";
 import { useAuthStore } from "@/store/auth";
 import supabase from "@/utils/supabase";
 import { Button, Spinner } from "@/components/ui";
@@ -7,7 +7,7 @@ import { Button, Spinner } from "@/components/ui";
 function AuthCallback() {
     // Zustand(또는 다른 상태 관리 라이브러리)에서 사용자 정보를 설정하는 함수를 가져옵니다.
     const setUser = useAuthStore((state) => state.setUser);
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     // 컴포넌트 마운트 시에만 실행되도록 useEffect의 의존성은 비워둡니다. ([])
     useEffect(() => {
